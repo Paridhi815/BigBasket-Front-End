6aa91c7fd330537474e5effe938ffcfd3dfad920
paridhi <paridhi815@gmail.com>
2018-03-15 09:19:35 +0530

    Initial Setup done

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "parser":"babel-eslint",
+    "extends": "airbnb",
+    "rules":{
+        "no-console":0,
+        "react/forbid-prop-types":0
+    },
+    "env": {
+        "browser": true
+      }
+};
---
+++ src/Components/App/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+
+// const Axios = require('axios');
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+      Paridhi
+      </div>
+    );
+  }
+}
+
+export default App;
